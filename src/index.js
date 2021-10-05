08466dce389ebfd860aff68403587048c2056ded
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-10-05 22:25:44 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-
 require('./db/mongoose');
-
-const userRouter = require("./routers/user")
-const taskRouter = require("./routers/task")
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
 
 app.use(express.json());
-
-const router = new express.Router()
-
-app.use(userRouter)
-app.use(taskRouter)
-
-
-
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log('Server is up on port http://localhost:' + port);
